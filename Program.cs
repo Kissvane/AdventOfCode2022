5ae910f6b2a2350b636e4f94c7ed7f04a2c3fdb5
Kissvane <adrien@nebulae.world>
2022-12-03 11:54:01 +0100

    [add] DAY 3

--- Program.cs
+++ Program.cs
@@ -5,13 +5,14 @@ namespace AdventCodeDay1
 {
     class Program
     {
-        public enum PLAY { ROCK = 0, PAPER = 1, SCISSORS = 2 };
         // See https://aka.ms/new-console-template for more information
         static void Main(string[] args)
         {
-            Day2();
+            Day3Part1();
+            Day3Part2();
         }
 
+        #region DAY1
         static void Day1()
         {
             string input = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "inputDay1.txt"));
@@ -48,8 +49,10 @@ namespace AdventCodeDay1
             //Console.WriteLine($"ELF {wantedElfIndex} CARRYING {maximum}");
             Console.WriteLine($"TOP 3 CARRYING {topThreeTotal}");
         }
+        #endregion
 
-
+        #region DAY2
+        public enum PLAY { ROCK = 0, PAPER = 1, SCISSORS = 2 };
         static void Day2()
         {
             string input = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "inputDay2.txt"));
@@ -161,6 +164,100 @@ namespace AdventCodeDay1
         {
             return VictoryPoint2(game) + CalculateNeededShapePoint(game);
         }
+        #endregion
+
+        #region DAY3
+
+        static void Day3Part1()
+        {
+            string input = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "test.txt"));
+
+            string[] array = input.Split("\n");
+            int result = 0;
+            foreach (string line in array)
+            {
+                string s1 = line.Substring(0,line.Length/2);
+                string s2 = line.Substring(line.Length / 2);
+                string intersection = Intersection(s1,s2);
+                Console.WriteLine($"{s1} {s2} {intersection}");
+                foreach (char c in intersection)
+                {
+                    result += CalculatePriority(c);
+                }
+            }
+
+            Console.Write(result);
+        }
+
+        static void Day3Part2()
+        {
+            string input = File.ReadAllText(Path.Combine(Environment.CurrentDirectory, "inputDay3.txt"));
+            string[] array = input.Split("\n");
+            int result = 0;
+            for(int i = 0; i < array.Length; i = i+3)
+            {
+                string s1 = array[i];
+                string s2 = array[i+1];
+                string s3 = array[i+2];
+                string intersection = Intersection(s1, s2, s3);
+                foreach (char c in intersection)
+                {
+                    result += CalculatePriority(c);
+                }
+            }
+
+            Console.Write($"RESULTAT {result}");
+        }
+
+        static string Intersection(params string[] args)
+        {
+            string intersection = "";
+            List<HashSet<char>> hashsets = new List<HashSet<char>>();
+
+            foreach (string s in args)
+            {
+                Console.WriteLine(s);
+                HashSet<char> hashset = new HashSet<char>();
+                foreach (char c in s)
+                {
+                    if(char.IsLetter(c)) hashset.Add(c);
+                }
+                hashsets.Add(hashset);
+            }
+
+            foreach (char c in hashsets[0])
+            {
+                bool add = true;
+                for (int i = 1; i < hashsets.Count; i++)
+                {
+                    if (!hashsets[i].Contains(c))
+                    {
+                        add = false;
+                        break;
+                    }
+                }
+
+                if(add) intersection += c;
+            }
+
+            Console.WriteLine(intersection);
+
+            return intersection;
+        }
+
+        static int CalculatePriority(char c)
+        {
+            if (char.IsUpper(c))
+            {
+                return (int)c - 38;
+            }
+            else
+            {
+                return (int)c - 96; 
+            }
+        }
+
+        #endregion
     }
 }
 
